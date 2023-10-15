70dadca794b7ea02c704c2b6c107576b76be25aa
Ubuntu <ubuntu@ip-172-31-25-85.eu-north-1.compute.internal>
2023-10-15 17:36:44 +0000

    teste

--- performancexcellence/performancexcellence/physiotherapy/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PhysiotherapyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'physiotherapy'
--- performancexcellence/performancexcellence/templatetags/custom_filters.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def remove_static(value):
-    return value.replace('static/', '', 1)
