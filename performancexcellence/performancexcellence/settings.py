4ecebed0342d9b60ece64b5ae9ec6a19e3c8b957
Samuel Remédios <samuelremedios@hotmail.com>
2023-10-15 19:31:54 +0100

    try to update settings

--- performancexcellence/performancexcellence/settings.py
+++ performancexcellence/performancexcellence/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -153,5 +152,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
